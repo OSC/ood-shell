748a4abdfd81a3189cb301071fbe2c3e0903669f
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-06-18 14:34:10 -0400

    revert fix for #48 bc hterm 1.85 fixes it

--- public/javascripts/ood_shell.js
+++ public/javascripts/ood_shell.js
@@ -1,50 +1,3 @@
-/**
- * Copy a string to the system clipboard.
- *
- * Note: If there is a selected range in the terminal, it'll be cleared.
- * 
- * @hacks Replacing this.document_ with the top-level document to ensure that copy on select works in Firefox
- * @hacks Removing the selection.extend behavior because it was preventing removal of the copy source
- *
- * @param {string} str The string to copy.
- */
-hterm.Terminal.prototype.copyStringToClipboard = function(str) {
-  if (this.prefs_.get('enable-clipboard-notice'))
-    setTimeout(this.showOverlay.bind(this, hterm.notifyCopyMessage, 500), 200);
-
-  var copySource = document.createElement('pre');
-  copySource.id = 'hterm:copy-to-clipboard-source';
-  copySource.textContent = str;
-  copySource.style.cssText = (
-      '-webkit-user-select: text;' +
-      '-moz-user-select: text;' +
-      'position: absolute;' +
-      'top: -99px');
-
-  document.body.appendChild(copySource);
-
-  var selection = document.getSelection();
-  var anchorNode = selection.anchorNode;
-  var anchorOffset = selection.anchorOffset;
-  var focusNode = selection.focusNode;
-  var focusOffset = selection.focusOffset;
-
-  selection.selectAllChildren(copySource);
-
-  hterm.copySelectionToClipboard(document);
-
-  // @FIXME after changing this.document_ to document selection's nodes are all null, causing an error
-  // This doesn't appear to break the application
-
-  // // IE doesn't support selection.extend. This means that the selection
-  // // won't return on IE.
-  // if (selection.extend) {
-  //   selection.collapse(anchorNode, anchorOffset);
-  //   selection.extend(focusNode, focusOffset);
-  // }
-
-  copySource.parentNode.removeChild(copySource);
-};
 
 // Object that defines a terminal element
 function OodShell(element, url, prefs) {
@@ -61,56 +14,50 @@ function OodShell(element, url, prefs) {
  * terminal is ready.) and setting preferences, etc.
  */
 OodShell.prototype.createTerminal = function () {
-  if(lib.resource.get('hterm/changelog/version', null).data === '1.85') {
-    
-    // Set backing store that hterm uses to read/write preferences
-    hterm.defaultStorage = new lib.Storage.Memory();
-    
-    // Create an instance of hterm.Terminal
-    this.term = new hterm.Terminal();
-    
-    var that = this;
-    
-    // Handler that fires when terminal is initialized and ready for use
-    this.term.onTerminalReady = function () {
-      // Create a new terminal IO object and give it the foreground.
-      // (The default IO object just prints warning messages about unhandled
-      // things to the JS console.)
-      var io = this.io.push();
-
-      // Set up event handlers for io
-      io.onVTKeystroke    = that.onVTKeystroke.bind(that);
-      io.sendString       = that.sendString.bind(that);
-      io.onTerminalResize = that.onTerminalResize.bind(that);
-
-      that.socket = new WebSocket(that.url);  // WS isn't made or bound until terminal is ready
-      //that.socket.onopen    = nothing, onopen callback bc the WS is the last thing to init
-      that.socket.onmessage = that.getMessage.bind(that);
-      that.socket.onclose   = that.closeTerminal.bind(that);
-    
-      // Capture all keyboard input
-      this.installKeyboard();
-    };
+  // Set backing store that hterm uses to read/write preferences
+  hterm.defaultStorage = new lib.Storage.Memory();
   
-    // Patch cursor setting
-    this.term.options_.cursorVisible = true;
-  
-    // Connect terminal to sacrificial DOM node
-    this.term.decorate(this.element);
+  // Create an instance of hterm.Terminal
+  this.term = new hterm.Terminal();
     
-    // Set preferences for terminal
-    for (var k in this.prefs) {
-      this.term.prefs_.set(k, this.prefs[k]);
-    }
+  var that = this;
     
-    // Warn user if he/she unloads page
-    window.onbeforeunload = function() {
-      return 'Leaving this page will terminate your terminal session.';
-    };
+  // Handler that fires when terminal is initialized and ready for use
+  this.term.onTerminalReady = function () {
+    // Create a new terminal IO object and give it the foreground.
+    // (The default IO object just prints warning messages about unhandled
+    // things to the JS console.)
+    var io = this.io.push();
   
-  } else {
-    document.getElementById('terminal').innerHTML = 'WARNING: version of hterm has changed. Please review ood_shell.js to ensure that the override of hterm.Terminal.prototype.copyStringToClipboard is still an appropriate fix to copy on select in Firefox.';
+    // Set up event handlers for io
+    io.onVTKeystroke    = that.onVTKeystroke.bind(that);
+    io.sendString       = that.sendString.bind(that);
+    io.onTerminalResize = that.onTerminalResize.bind(that);
+    
+    that.socket = new WebSocket(that.url);  // WS isn't made or bound until terminal is ready
+    //that.socket.onopen    = nothing left to do, no onopen callback bc the WS is the last thing to init
+    that.socket.onmessage = that.getMessage.bind(that);
+    that.socket.onclose   = that.closeTerminal.bind(that);
+    
+    // Capture all keyboard input
+    this.installKeyboard();
+  };
+    
+  // Patch cursor setting
+  this.term.options_.cursorVisible = true;
+    
+  // Connect terminal to sacrificial DOM node
+  this.term.decorate(this.element);
+    
+  // Set preferences for terminal
+  for (var k in this.prefs) {
+    this.term.prefs_.set(k, this.prefs[k]);
   }
+    
+  // Warn user if he/she unloads page
+  window.onbeforeunload = function() {
+    return 'Leaving this page will terminate your terminal session.';
+  };
 };
 
 OodShell.prototype.getMessage = function (ev) {
