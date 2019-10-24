037f11d774032a95c219ec7884cb2ddac823c16f
Samir <mansour.69@osu.edu>
2019-10-24 12:39:58 -0400

    add sessions

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var pty       = require('node-pty');
 var hbs       = require('hbs');
 var dotenv    = require('dotenv');
 var port      = 3000;
+var uuidv4    = require('uuid/v4');
+var uuid;
 
 // Read in environment variables
 dotenv.config({path: '.env.local'});
@@ -18,6 +20,76 @@ if (process.env.NODE_ENV === 'production') {
 if (fs.existsSync('.env')) {
   console.warn('[DEPRECATION] The file \'.env\' is being deprecated. Please move this file to \'/etc/ood/config/apps/shell/env\'.');
   dotenv.config({path: '.env'});
+}
+
+//Create terminals object
+var terminals = {
+  instances: {
+
+  },
+
+  create: function() {
+      var host = process.env.DEFAULT_SSHHOST || 'localhost';
+      var dir;
+      var term;
+      var cmd, args;
+      
+      uuid = uuidv4();
+      cmd = 'ssh';
+      args = dir ? [host, '-t', 'cd \'' + dir.replace(/\'/g, "'\\''") + '\' ; exec ${SHELL} -l'] : [host];
+
+      this.instances[uuid] = pty.spawn(cmd, args, {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30
+      });
+
+      return uuid;
+  },
+
+  exists: function() {
+    if (uuid in this.instances) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  get: function(uuid) {
+    return this.instances[uuid];
+  },
+
+  attach: function(uuid, ws) {
+    var term = this.get(uuid);
+
+    console.log('Opened terminal: ' + term.pid);
+
+    term.on('data', function (data) {
+      ws.send(data, function (error) {
+        if (error) console.log('Send error: ' + error.message);
+      });
+    });
+
+    term.on('error', function (error) {
+      ws.close();
+    });
+
+    term.on('close', function () {
+      ws.close();
+    });
+
+    ws.on('message', function (msg) {
+      msg = JSON.parse(msg);
+      if (msg.input)  term.write(msg.input);
+      if (msg.resize) term.resize(parseInt(msg.resize.cols), parseInt(msg.resize.rows));
+    });
+
+    ws.on('close', function () {
+      term.end();
+      console.log('Closed terminal: ' + term.pid);
+    });
+
+  }
 }
 
 // Create all your routes
@@ -46,10 +118,7 @@ var wss = new WebSocket.Server({ server: server });
 
 wss.on('connection', function connection (ws) {
   var match;
-  var host = process.env.DEFAULT_SSHHOST || 'localhost';
-  var dir;
-  var term;
-  var cmd, args;
+
 
   console.log('Connection established');
 
@@ -59,41 +128,12 @@ wss.on('connection', function connection (ws) {
     if (match[2]) dir = decodeURIComponent(match[2]);
   }
 
-  cmd = 'ssh';
-  args = dir ? [host, '-t', 'cd \'' + dir.replace(/\'/g, "'\\''") + '\' ; exec ${SHELL} -l'] : [host];
+  if (terminals.exists() === false) {
+    terminals.create();
+  }
+  
+  terminals.attach(uuid, ws);
 
-  term = pty.spawn(cmd, args, {
-    name: 'xterm-256color',
-    cols: 80,
-    rows: 30
-  });
-
-  console.log('Opened terminal: ' + term.pid);
-
-  term.on('data', function (data) {
-    ws.send(data, function (error) {
-      if (error) console.log('Send error: ' + error.message);
-    });
-  });
-
-  term.on('error', function (error) {
-    ws.close();
-  });
-
-  term.on('close', function () {
-    ws.close();
-  });
-
-  ws.on('message', function (msg) {
-    msg = JSON.parse(msg);
-    if (msg.input)  term.write(msg.input);
-    if (msg.resize) term.resize(parseInt(msg.resize.cols), parseInt(msg.resize.rows));
-  });
-
-  ws.on('close', function () {
-    term.end();
-    console.log('Closed terminal: ' + term.pid);
-  });
 });
 
 server.listen(port, function () {
